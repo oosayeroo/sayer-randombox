44f1050425bf2d83a4b2cc704ebc59a19fb39be3
oosayeroo <98313914+oosayeroo@users.noreply.github.com>
2022-07-26 22:32:02 +0100

    Add files via upload

--- client/main.lua
+++ client/main.lua
@@ -68,6 +68,44 @@ AddEventHandler("qb-randombox:CaseOpening", function()
     end)
 end)
 
+RegisterNetEvent("qb-randombox:BagOpening")
+AddEventHandler("qb-randombox:BagOpening", function()
+    local playerPed = PlayerPedId()
+    local coords    = GetEntityCoords(playerPed)
+    local forward   = GetEntityForwardVector(playerPed)
+    local x, y, z   = table.unpack(coords + forward * 1.0)
+
+    local bag = `hei_prop_heist_binbag`
+    RequestModel(bag)
+    while (not HasModelLoaded(bag)) do
+        Wait(1)
+    end
+    local bag1 = CreateObject(bag, x, y, z, true, false, true)
+    PlaceObjectOnGroundProperly(bag1)
+    SetEntityAsMissionEntity(bag1)
+
+    TriggerEvent('animations:client:EmoteCommandStart', {"mechanic3"})
+    QBCore.Functions.Progressbar('name_here', 'Opening Bag...', 5000, false, true, {
+        disableMovement = true,
+        disableCarMovement = true,
+        disableMouse = false,
+        disableCombat = true,
+    }, {}, {}, {}, function()
+        TriggerEvent('animations:client:EmoteCommandStart', {"c"})
+
+        DeleteEntity(bag1)
+
+        TriggerServerEvent('QBCore:Server:RemoveItem', 'randombag', 1)
+        TriggerEvent("inventory:client:ItemBox", QBCore.Shared.Items['randombag'], "remove")
+
+        TriggerServerEvent('qb-randombox:server:GetRewardBag')
+    end)
+end)
+
+RegisterNetEvent('qb-randombox:client:SetBagItems', function(BagItemList)
+	Config.BagItemList = BagItemList
+end)
+
 RegisterNetEvent('qb-randombox:client:SetBoxItems', function(BoxItemList)
 	Config.BoxItemList = BoxItemList
 end)
