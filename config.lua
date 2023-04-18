da78f37f42b7b8ba64f799435ae1db5cecd0d6a1
oosayeroo <98313914+oosayeroo@users.noreply.github.com>
2023-04-18 22:11:47 +0100

    V1.5.0 - Code Improvements

--- config.lua
+++ config.lua
@@ -1,132 +1,142 @@
 Config = {}
 ---Box and Case Can have completely different items within them. they do not inteferre with each other at all.
 
-Config.OpeningEmote = "mechanic3"  --uses dpemotes
-
-Config.NumberOfItemsBox = 2  --amount of different items to receive from 1 (box) 
-Config.NumberOfItemsCase = 2  --amount of different items to receive from 1 (Case) 
-Config.NumberOfItemsBag = 2
-Config.NumberOfItemsAmmo = 1
-Config.NumberOfItemsMedkit = 1
-Config.NumberOfItemsGun = 1
-Config.NumberOfItemsGym = 1
-
-Config.ItemAmountBox = 5  --quantity of each item received (1 coffee or 2 coffee etc) (Box)
-Config.ItemAmountCase = 5  --quantity of each item received (1 coffee or 2 coffee etc) (Case)
-Config.ItemAmountBag = 5
-Config.ItemAmountAmmo = 1
-Config.ItemAmountMedkit = 1
-Config.ItemAmountGun = 1
-Config.ItemAmountGym = 1
-
-Config.LuckItemBox = "trojan_usb"  --change to whatever you want (Box)
-Config.LuckItemCase = "bandage"  --change to whatever you want (Case)
-Config.LuckItemBag = "lockpick"
-
-Config.LuckAmountBox = 1 --amount of luck item you receive if enabled (Box)
-Config.LuckAmountCase = 1 --amount of luck item you receive if enabled (Case)
-Config.LuckAmountBag = 1
-
----///DONATOR STUFF//---
---Config.NumberOfItemsDonate1 = 2
---Config.ItemAmountDonate1 = 5
---Config.LuckItemDonate = "lockpick" --change to a rare item
---Config.LuckAmountDonate = 1
---Config.DonateItemList = {}
---Config.DonateItems = {  --change these to whatever
---    "coffee",
---    "advancedlockpick",
---}
-
-Config.BoxItemList = {}  --dont touch
-
-Config.BoxItems = {  --add as many items as you like. these are the random items you may get
-    "coffee",
-    "twerks_candy",
-    "lockpick",
-    "advancedlockpick",
-    "weapon_dagger",
-    "weapon_flashlight",
-    "weapon_briefcase",
-    "weapon_briefcase_02",
-    "weapon_pistol_mk2",
-    "pistol_ammo",
-    "security_card_01",
-    "beer",
-    "whiskey",
-    "joint",
-    "metalscrap",
-    "repairkit",
-}
-
-Config.CaseItemList = {}  --dont touch
-
-Config.CaseItems = {   --place whatever items you want in here. theres no limit
-    "casinochips",
-    "lighter",
-    "empty_evidence_bag",
-    "handcuffs",
-    "rolex",
-    "goldchain",
-    "plastic",
-    "weed_white-widow",
-    "joint",
-}
-
-Config.BagItemList = {}
-
-Config.BagItems = {   --place whatever items you want in here. theres no limit
-    "pistol_ammo",
-    "rifle_ammo",
-    "smg_ammo",
-    "shotgun_ammo",
-    "security_card_01",
-    "water_bottle",
-    "cokebaggy",
-    "weed_white-widow",
-    "joint",
-}
-
-Config.AmmoItemList = {}
-
-Config.AmmoItems = {
-    "pistol_ammo",
-    "rifle_ammo",
-    "smg_ammo",
-    "shotgun_ammo",
-    "mg_ammo",
-}
-
-Config.MedkitItemList = {}
-
-Config.MedkitItems = {
-    "firstaid",
-    "bandage",
-    "ifaks",
-    "painkillers",
-}
-
-Config.GunItemList = {}
-
-Config.GunItems = {
-    "weapon_pistol",
-    "weapon_pistol50",
-    "weapon_pistol_mk2",
-    "weapon_smg",
-    "weapon_pumpshotgun",
-    "weapon_molotov",
-    "pistol_extendedclip",
-    "pistol_flashlight",
-    "pistol50_extendedclip",
-    "pistol50_luxuryfinish",
-    "smg_extendedclip",
-    "pumpshotgun_luxuryfinish",
-}
-
-Config.GymItemList = {}
-
-Config.GymItems = {
-    "water_bottle",
-    "grapejuice",
-    "snikkel_candy",
+Config.Useables = {
+    ['randombox'] = { --item code
+        Model = `hei_prop_heist_box`, --model that spawns infront of player
+        OpeningEmote = "mechanic3", --emote used to open
+        OpeningTime = 5, --in seconds
+        NumberOfItems = 2, --amount of different items to receive
+        ItemAmount = 1, --amount of specific items to get
+        Items = {
+            "coffee",
+            "twerks_candy",
+            "lockpick",
+            "advancedlockpick",
+            "weapon_dagger",
+            "weapon_flashlight",
+            "weapon_briefcase",
+            "weapon_briefcase_02",
+            "weapon_pistol_mk2",
+            "pistol_ammo",
+            "security_card_01",
+            "beer",
+            "whiskey",
+            "joint",
+            "metalscrap",
+            "repairkit",
+        },
+        HasRare = true,
+        RareItemChance = 5, --in % out of 100
+        RareItemAmount = 1, 
+        RareItem = "trojan_usb",
+    },
+    ['randomcase'] = {
+        Model = `bkr_prop_biker_case_shut`,
+        OpeningEmote = "mechanic3",
+        OpeningTime = 5, --in seconds
+        NumberOfItems = 2, --amount of different items to receive
+        ItemAmount = 1, --amount of specific items to get
+        Items = {
+            "casinochips",
+            "lighter",
+            "empty_evidence_bag",
+            "handcuffs",
+            "rolex",
+            "goldchain",
+            "plastic",
+            "weed_white-widow",
+            "joint",
+        },
+        HasRare = true,
+        RareItemChance = 5, --in % out of 100
+        RareItemAmount = 1, 
+        RareItem = "bandage",
+    },
+    ['randombag'] = {
+        Model = `hei_prop_heist_binbag`,
+        OpeningEmote = "mechanic3",
+        OpeningTime = 5, --in seconds
+        NumberOfItems = 2, --amount of different items to receive
+        ItemAmount = 1, --amount of specific items to get
+        Items = {
+            "pistol_ammo",
+            "rifle_ammo",
+            "smg_ammo",
+            "shotgun_ammo",
+            "security_card_01",
+            "water_bottle",
+            "cokebaggy",
+            "weed_white-widow",
+            "joint",
+        },
+        HasRare = true,
+        RareItemChance = 5, --in % out of 100
+        RareItemAmount = 1, 
+        RareItem = "lockpick",
+    },
+    ['randomammo'] = {
+        Model = `prop_box_ammo01a`,
+        OpeningEmote = "mechanic3",
+        OpeningTime = 5, --in seconds
+        NumberOfItems = 2, --amount of different items to receive
+        ItemAmount = 1, --amount of specific items to get
+        Items = {
+            "pistol_ammo",
+            "rifle_ammo",
+            "smg_ammo",
+            "shotgun_ammo",
+            "mg_ammo",
+        },
+        HasRare = false,
+    },
+    ['randommedkit'] = {
+        Model = `xm_prop_smug_crate_s_medical`,
+        OpeningEmote = "mechanic3",
+        OpeningTime = 5, --in seconds
+        NumberOfItems = 2, --amount of different items to receive
+        ItemAmount = 1, --amount of specific items to get
+        Items = {
+            "firstaid",
+            "bandage",
+            "ifaks",
+            "painkillers",
+        },
+        HasRare = false,
+    },
+    ['randomgun'] = {
+        Model = `prop_box_guncase_03a`,
+        OpeningEmote = "mechanic3",
+        OpeningTime = 5, --in seconds
+        NumberOfItems = 2, --amount of different items to receive
+        ItemAmount = 1, --amount of specific items to get
+        Items = {
+            "weapon_pistol",
+            "weapon_pistol50",
+            "weapon_pistol_mk2",
+            "weapon_smg",
+            "weapon_pumpshotgun",
+            "weapon_molotov",
+            "pistol_extendedclip",
+            "pistol_flashlight",
+            "pistol50_extendedclip",
+            "pistol50_luxuryfinish",
+            "smg_extendedclip",
+            "pumpshotgun_luxuryfinish",
+        },
+        HasRare = false,
+    },
+    ['randomgym'] = {
+        Model = `hei_p_m_bag_var22_arm_s`,
+        OpeningEmote = "mechanic3",
+        OpeningTime = 5, --in seconds
+        NumberOfItems = 2, --amount of different items to receive
+        ItemAmount = 1, --amount of specific items to get
+        Items = {
+            "water_bottle",
+            "grapejuice",
+            "snikkel_candy",
+        },
+        HasRare = false,
+    },
 }
